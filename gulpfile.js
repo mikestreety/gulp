c9b6201bdce7d0cd8249fbcf91c4dec96c83f453
Mike Street <mikestreety@gmail.com>
2014-06-02 17:04:41 +0100

    Add firefox 4 to autoprefixer for gradient

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ gulp.task('css', function(){
 
 	return es.concat(gulp.src(vendorFiles.styles), sassFiles)
 		.pipe(plugins.concat('style.min.css'))
-		.pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4', 'Firefox >= 4')))
 		.pipe(isProduction ? plugins.combineMediaQueries({
 			log: true
 		}) : gutil.noop())
